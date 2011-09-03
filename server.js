db235262dccb09edac861c5300589b3611ef6fd3
Gabriel Farrell <g@grrawr.com>
2011-09-03 01:47:36 -0400

    Added data-check route

--- server.js
+++ server.js
@@ -14,7 +14,56 @@ app.route('/', function(req, res) {
   //  content: app.templates.index
   //};
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(whiskers.render(app.templates.base, context));//, partials));
+  res.end(whiskers.render(app.templates['base.html'], context));//, partials));
+});
+
+app.route('/data-check', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var message = {};
+  var repoOptions = {
+    host: 'data.free103point9.org',
+    port: 80,
+    path: '/log'
+  };
+  var repoLog = '';
+  var repoReq = http.get(repoOptions, function(repoRes) {
+    repoRes.on('data', function(chunk) {
+      repoLog += chunk;
+    });
+    repoRes.on('end', function() {
+      if (repoRes.statusCode == 200) {
+        message['log retrieved'] = true;
+      }
+      console.log('repo log: ' + repoLog);
+      res.end(JSON.stringify(message));
+    });
+  });
+  //var esOptions = {
+  //  host: '127.0.0.1',
+  //  port: 9200,
+  //  path: '/ta/artist/_search',
+  //  method: 'POST'
+  //};
+  //var data = '';
+  //var esReq = http.request(esOptions, function(esRes) {
+  //  esRes.on('data', function(chunk) {
+  //    console.log('body: ' + chunk);
+  //    data += chunk;
+  //  });
+  //  esRes.on('end', function() {
+  //    var result = JSON.parse(data);
+  //    context['artists'] = [];
+  //    for (var i=0, l=result.hits.total, artist; i<l; i++) {
+  //      artist = result.hits.hits[i].fields;
+  //      context['artists'].push(artist);
+  //    }
+  //    res.writeHead(200, {'Content-Type': 'text/html'});
+  //    res.end(whiskers.render(app.templates['base.html'], context, partials));
+  //  });
+  //});
+  //esReq.on('error', function(e) {
+  //  console.log("Got error: " + e.message);
+  //});
 });
 
 exports.start = function(port) {
