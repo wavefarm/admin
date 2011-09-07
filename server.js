37f47c34f8d2b33e4caad558177e9d732e1ec346
Gabriel Farrell <g@grrawr.com>
2011-09-06 22:03:29 -0400

    Get meta.json docs and insert into index

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
-var Lazy = require('lazy');
+var lazy = require('lazy');
+var pairtree = require('pairtree');
 var querystring = require('querystring');
 var snout = require('snout');
 var sys = require('sys');
@@ -18,31 +19,85 @@ app.route('/', function(req, res) {
   res.end(whiskers.render(app.templates['base.html'], context));//, partials));
 });
 
+var timestampRe = /^/gm;
 app.route('/data-check', function(req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  var message = {};
   var repoOptions = {
     host: 'data.free103point9.org',
     port: 80,
-    path: '/log'
+    path: '/r/.logs/action.log'
   };
   var repoLog = '';
   var repoReq = http.get(repoOptions, function(repoRes) {
-    var lazy = new Lazy(repoRes);
-    var count = 0;
-    lazy.lines.forEach(function(line) {
-      count++;
-      console.log(count + ' ' + line);
+    if (repoRes.statusCode != 200) {
+      res.end(JSON.stringify({'repo status': repoRes.statusCode, 'log retrieved': false}));
+      return;
+    }
+    var action, timestamp, method, id, filename;
+    lazy(repoRes).lines.forEach(function(line) {
+      line = String(line);
+      action = line.split(' ');
+      timestamp = action.shift();
+      method = action.shift();
+      id = action.shift();
+      filename = action.join(' ');
+      console.log(filename);
+      if (filename == 'meta.json') {
+        if (method == 'POST') {
+          console.log('Adding ' + id);
+          // get meta.json
+          var metaOptions = {
+            host: 'data.free103point9.org',
+            port: 80,
+            path: '/r' + pairtree.path(id) + 'meta.json'
+          };
+          http.get(metaOptions, function(metaRes) {
+            var meta = '';
+            metaRes.on('data', function(chunk) {
+              meta += chunk;
+            });
+            metaRes.on('end', function() {
+              var doc = JSON.parse(meta);
+              // add to index
+              var esOptions = {
+                host: '127.0.0.1',
+                port: 9200,
+                path: '/ta/' + doc.type + '/' + doc._id,
+                method: 'PUT'
+              };
+              var esReq = http.request(esOptions, function(esRes) {
+                var data = '';
+                esRes.on('data', function(chunk) {
+                  //console.log('body: ' + chunk);
+                  data += chunk;
+                });
+                esRes.on('end', function() {
+                  console.log(data);
+                });
+                esReq.on('error', function(e) {
+                  console.log("ES error: " + e.message);
+                });
+              });
+              esReq.end(meta);
+            });
+          });
+        } else if (method == 'DELETE') {
+          console.log('Dropping ' + id);
+          // drop from index
+        }
+      }
     });
     //repoRes.on('data', function(chunk) {
+    //  console.log('\n\nChunk:\n'+chunk);
     //  repoLog += chunk;
+    //  // look in data received so far for last-checked timestamp
+    //  if (repoLog.indexOf('2011-09-05T21:41:55.158363') != -1) {
+    //    console.log('timestamp found');
+    //  }
     //});
     repoRes.on('end', function() {
-      if (repoRes.statusCode == 200) {
-        message['log retrieved'] = true;
-      }
       //console.log('repo log: ' + repoLog);
-      res.end(JSON.stringify(message));
+      res.end(JSON.stringify({'log retrieved': true}));
     });
   });
   //var esOptions = {
