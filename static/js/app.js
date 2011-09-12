df8e4d8f19935c169ceb1a37ba81c53179e9761f
Gabriel Farrell <g@grrawr.com>
2011-09-12 16:17:29 -0400

    Moved most static js into app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,73 @@
-// Populate window.app
+// Set up app
+var app;
+// Use lower-level ajax function to set async to false 
+$.ajax({
+  url: '_app', 
+  async: false,
+  appType: 'json',
+  success: function(data) {
+    app = data;
+    app.routes = [];
+    // Add routing functionality
+    app.route = function(re, cb) {
+      if (typeof re == 'string') re = new RegExp('^'+re+'$');
+      app.routes.push({re: re, cb: cb});
+    };
+    app.load = function(hash) {
+      var hashArr = hash.split('?');
+      hash = hashArr.shift();
+      var query = $.deparam(hashArr.join('?'));
+      var route, ret404 = true;
+      for (var i=0, l=app.routes.length; i<l; i++) {
+        route = app.routes[i];
+        if (route.re.test(hash)) {
+          ret404 = false;
+          // return capture groups as second param
+          route.cb(query, route.re.exec(hash));
+          break;
+        }
+      }
+      if (ret404) {
+        $('#main').html('<p>404</p>');
+      }
+    };
+  }
+});
 
-(function(global) {
-  // Use lower-level ajax function to set async to false 
-  // so this is available on page load
-  $.ajax({
-    url: '_app', 
-    async: false,
-    dataType: 'json',
-    success: function(data) {
-      global.app = data;
-    }
+// routes
+app.route('', function() {
+  window.location = '#dash';
+});
+app.route('dash', function(query) {
+  $('#main').html(whiskers.render(app.templates.dash));
+  if (query.q) {
+    $('form.search input[name=q]').val(query.q);
+  }
+});
+app.route('dash/([0-9a-f]{7})', function(query, captures) {
+  $('#main').html(whiskers.render(app.templates.item));
+});
+app.route('models', function() {
+  $('#main').html('<section class="models"><h1>Models</h1></section>');
+});
+app.route('templates', function() {
+  $('#main').html('<section class="templates"><h1>Templates</h1></section>');
+});
+app.route('users', function() {
+  $('#main').html('<section class="users"><h1>Users</h1></section>');
+});
+
+// let's roll
+$(function() {
+  $(window).bind('hashchange', function(e) {
+    var hash = e.fragment;
+    app.load(hash);
   });
-})(this);
+  $(window).trigger('hashchange');
+});
+
+// dashboard js
+$('form.search').live('submit', function() {
+  $.bbq.pushState('#dash?q='+encodeURIComponent($(this).find('input[name=q]').val()));
+  return false;
+});
--- static/js/base.js
+++ static/js/base.js
@@ -1,29 +1,7 @@
 $(function() {
-  $('form.search').live('submit', function() {
-    $.bbq.pushState('#q='+encodeURIComponent($(this).find('input[name=q]').val()));
-    return false;
-  });
   $(window).bind('hashchange', function(e) {
     var hash = e.fragment;
-    var routes = {
-      'templates': function() {
-        $('#main').html('<section class="templates"><h1>Templates</h1></section>');
-      },
-      'models': function() {
-        $('#main').html('<section class="models"><h1>Models</h1></section>');
-      },
-      'users': function() {
-        $('#main').html('<section class="users"><h1>Users</h1></section>');
-      }
-    };
-    if (routes[hash]) {
-      routes[hash]();
-    } else {
-      if (hash.indexOf('q=') == 0) {
-        $('form.search input[name=q]').val(decodeURIComponent(hash.substr(2)));
-      }
-      $('#main').html(whiskers.render(app.templates.search));
-    }
+    app.load(hash);
   });
   $(window).trigger('hashchange');
 });
