8517080900a569103e0b82d25c6e5416d0c36c32
Gabriel Farrell <g@grrawr.com>
2012-10-15 23:48:49 -0400

    Begin rewrite

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-var app = require('./app');
-var http = require('http');
+var fs = require('fs')
+var http = require('http')
+var rut = require('rut')
+var send = require('send')
+var stack = require('stack')
 
-var port = process.argv[2] || 8126;
+var port = process.argv[2] || process.env.PORT || 1039
 
-http.createServer(function(req, res) {
-  app.respond(req, res);
-}).listen(port);
-
-console.log('Running on port '+port);
+http.createServer(stack(
+  rut.get('/', function (req, res, next) {
+    fs.readFile('static/templates/base.html', function (err, data) {
+      if (err) next(err)
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.end(data)
+    })
+  }),
+  rut.get('/**', function (req, res, next, pathname) {
+    send(req, pathname).from('static').pipe(res)
+  })
+)).listen(port, function () {
+  console.log('Listening on port '+port)
+})
