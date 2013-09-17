87838ebb9ca26f44233ceeb4dd0411bd2f662ad0
Gabriel Farrell <g@grrawr.com>
2013-09-17 02:01:11 -0400

    Schema brought into item edit. Refs #1

--- routes/itemGet.js
+++ routes/itemGet.js
@@ -1,59 +1,75 @@
-var api = require('../api')
-var hyperglue = require('hyperglue')
-var fs = require('fs')
+var api = require('../api');
+var hyperglue = require('hyperglue');
+var fs = require('fs');
+
+
+function defaultRender (type) {
+  return function (name, value) {
+    return hyperglue('<div class="label"><label></label></div><div class="input"><input type="' + (type || 'text') + '"></div>', {
+      label: {
+        'for': name + 'Input',
+        _text: name
+      },
+      '.input input': {
+        id: name + 'Input',
+        name: name,
+        value: String(value)
+      }
+    });
+  };
+}
+
+var inputMap = {
+  'boolean': function (name, value) {
+    var input = {id: name + 'Input', name: name};
+    if (value) input.checked = 'checked';
+    return hyperglue('<label></label> <input type="checkbox">', {
+      label: {
+        'for': name + 'Input',
+        _text: name
+      },
+      input: input
+    });
+  },
+  date: defaultRender('date'),
+  text: function (name, value) {
+    return hyperglue('<div class="label"><label></label></div><textarea></textarea>', {
+      label: {
+        'for': name + 'Input',
+        _text: name
+      },
+      textarea: {
+        id: name + 'Input',
+        name: name,
+        _text: value
+      }
+    })
+  },
+  url: defaultRender('url')
+};
+
+function fieldWidget (item, schema) {
+  return function (fieldName) {
+    var field = schema.fields[fieldName];
+    var renderInput = inputMap[field.type] || defaultRender();
+    return {div: {_html: renderInput(fieldName, item[fieldName]).innerHTML}};
+  };
+}
 
 module.exports = function (req, res, next, id) {
   api.get(id, function (err, apiRes, item) {
     if (err) return next(err)
     if (apiRes.statusCode == 404) return next()
-    // TODO handle unknown type
-    var schema = req.schemas[item.type]
-    var inputMap = {
-      'boolean': function (name, value) {
-        var data = {id: name + 'Input'}
-        if (value) data.checked = 'checked'
-        return hyperglue('<input type="checkbox">', {input: data})
-      },
-      'date': function (name, value) {
-        return hyperglue('<input type="date">', {
-          input: {
-            id: name + 'Input',
-            value: value
-          }
-        })
-      },
-      'text': function (name, value) {
-        return hyperglue('<textarea></textarea>', {
-          textarea: {
-            id: name + 'Input',
-            _text: value
-          }
-        })
-      }
-    }
+    var schema = req.schemas[item.type];
+    var fields = schema && schema.item.map(fieldWidget(item, schema));
     res.render('item.html', {
       title: item.main,
+      '.item-id span': item.id,
       '.item-name': item.main,
-      '.raw': JSON.stringify(item, null, 2),
-      '.field': schema && schema.fields.map(function (field) {
-        var renderInput = inputMap[field.type] || function (name, value) {
-          return hyperglue('<input type="text">', {
-            input: {
-              id: name + 'Input',
-              value: String(value)
-            }
-          })
-        }
-        return {
-          label: {
-            'for': field.name + 'Input',
-            _text: field.name
-          },
-          '.input': {
-            _html: renderInput(field.name, item[field.name]).innerHTML
-          }
-        }
-      })
+      '.item-type span': item.type,
+      '#raw': JSON.stringify(item, null, 2),
+      'input[name=raw]': {value: JSON.stringify(item, null, 2)},
+      '.form-field': fields
     })
   })
 }
--- routes/itemPost.js
+++ routes/itemPost.js
@@ -1,9 +1,43 @@
 var api = require('../api')
 var fs = require('fs')
 
+function defaultValidator (value) {
+  return true;
+}
+
+var validatorMap = {
+};
+
+function defaultSave (item, field, value) {
+  item[field] = String(value);
+}
+
+// TODO Handle complex field types
+var saveMap = {
+  'boolean': function (item, field, value) {
+    item[field] = !!value;
+  }
+};
+
 module.exports = function (req, res, next, id) {
-  var item = JSON.parse(req.parsedBody.raw)
-  api.put(id, item, function (err, apiRes, item) {
+  var item = JSON.parse(req.parsedBody.raw);
+  var schema = req.schemas[item.type];
+  //console.log(req.parsedBody)
+  schema.item.forEach(function (fieldName) {
+    var field = schema.fields[fieldName];
+    var validator = validatorMap[field.type] || defaultValidator;
+    var save = saveMap[field.type] || defaultSave;
+    var value = req.parsedBody[fieldName];
+    if (validator(value)) {
+      save(item, fieldName, value);
+      if (schema.main === fieldName) save(item, 'main', value);
+    } else {
+      // Render the form and display error
+    }
+  });
+  console.log(item)
+  //return res.end()
+  api.put(id, item, function (err, apiRes, newItem) {
     if (err) return next(err)
     res.statusCode = 303
     res.setHeader('location', '/' + id)
