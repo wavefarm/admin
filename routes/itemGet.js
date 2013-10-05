46be74380097ebd16347e2e7bbb43f6405883461
Gabriel Farrell <g@grrawr.com>
2013-10-04 23:58:02 -0400

    Post relying more on API and not redirecting on errors. Refs #2

--- routes/itemGet.js
+++ routes/itemGet.js
@@ -4,15 +4,17 @@ var fields = require('../fields');
 
 module.exports = function (req, res, next, id) {
   api.get(id, function (err, apiRes, item) {
-    if (err) return next(err)
-    if (apiRes.statusCode == 404) return next()
+    if (err) return next(err);
+    if (apiRes.statusCode == 404) return next();
+    var raw = JSON.stringify(item, null, 2);
     var schema = req.schemas[item.type];
     res.render('item.html', {
       title: item.main,
       '.item-id span': item.id,
       '.item-name': item.main,
       '.item-type span': item.type,
-      '#raw': JSON.stringify(item, null, 2),
+      '#raw': raw,
+      'input[name=raw]': {value: raw},
       '.form-field': fields(item, schema)
     })
   })
--- routes/itemPost.js
+++ routes/itemPost.js
@@ -1,44 +1,54 @@
 var api = require('../api')
+var fields = require('../fields');
 var fs = require('fs')
 
-function defaultValidator (value) {
-  return true;
+function defaultClean (value) {
+  return String(value);
 }
 
-var validatorMap = {
-};
-
-function defaultSave (item, field, value) {
-  item[field] = String(value);
+function relClean (value) {
+  return value.split(/\s+/).map(function (id) {
+    return {id: id};
+  });
 }
 
-// TODO Handle complex field types
-var saveMap = {
-  'boolean': function (item, field, value) {
-    item[field] = !!value;
+var cleanMap = {
+  'boolean': function (value) {
+    return !!value;
   }
 };
+
+function getClean (type) {
+  var clean;
+  if (type && type.indexOf('rel:') == 0) clean = relClean;
+  else clean = cleanMap[type] || defaultClean;
+  return clean;
+}
 
 module.exports = function (req, res, next, id) {
   var item = JSON.parse(req.parsedBody.raw);
   var schema = req.schemas[item.type];
-  //console.log(req.parsedBody)
   schema.item.forEach(function (fieldName) {
     var field = schema.fields[fieldName];
-    var validator = validatorMap[field.type] || defaultValidator;
-    var save = saveMap[field.type] || defaultSave;
     var value = req.parsedBody[fieldName];
-    if (validator(value)) {
-      save(item, fieldName, value);
-      if (schema.main === fieldName) save(item, 'main', value);
-    } else {
-      // Render the form and display error
-    }
+    var clean = getClean(field.type);
+    item[fieldName] = clean(value);
   });
-  console.log(item)
-  //return res.end()
   api.put(id, item, function (err, apiRes, newItem) {
     if (err) return next(err)
+    if (apiRes.statusCode == 400) {
+      // Problem saving. Render the form and display the errors.
+      var raw = JSON.stringify(item, null, 2);
+      return res.render('item.html', {
+        title: item.main,
+        '.item-id span': item.id,
+        '.item-name': item.main,
+        '.item-type span': item.type,
+        '#raw': raw,
+        'input[name=raw]': {value: raw},
+        '.form-field': fields(item, schema)
+      })
+    }
     res.statusCode = 303
     res.setHeader('location', '/' + id)
     res.end()
