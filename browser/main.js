7f1c6e988363b7a390859f4eedf48a88589c8e37
Gabriel Farrell <g@grrawr.com>
2014-02-20 23:12:52 -0500

    Basic displaying for related fields widget. Refs #18

---
+++ browser/main.js
@@ -0,0 +1,19 @@
+var rels = $('.rel');
+
+rels.each(function (i, rel) {
+  rel = $(rel);
+  rel.hide();
+  var formField = rel.parent();
+  var relItems = rel.val().split('\n').filter(function (line) {
+    return line;
+  }).map(function (line) {
+    return {
+      id: line.substr(0, 6),
+      main: line.substr(7).trim()
+    }
+  });
+  relItems.forEach(function (item) {
+    formField.append('<div class="rel-item"><a href="/'+item.id+'" target="_blank">'+item.main+'</a> <a href="">remove</a></div>');
+  });
+  formField.append('<div class="rel-add"><input type="text"> <a href="">add</a></div>');
+});
