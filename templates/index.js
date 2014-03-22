5d25a469062d9262d0be9c24589e52743398b788
Gabriel Farrell <g@grrawr.com>
2014-03-21 23:29:03 -0400

    Include prepended slash when looking up template

--- templates/index.js
+++ templates/index.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 
 var t, tPath
 module.exports = function (tName) {
-  tPath = __dirname + tName
+  tPath = __dirname + '/' + tName
   if (!fs.existsSync(tPath)) return ''
   t = fs.createReadStream(tPath, {encoding: 'utf8'})
   t.stats = fs.statSync(tPath)
