c7be6532e8258ca8f9138fa88b9ae5287469795a
Gabriel Farrell <g@grrawr.com>
2014-03-21 22:58:52 -0400

    Add template piping function. Refs #25

---
+++ templates/index.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+
+var t, tPath
+module.exports = function (tName) {
+  tPath = __dirname + tName
+  if (!fs.existsSync(tPath)) return ''
+  t = fs.createReadStream(tPath, {encoding: 'utf8'})
+  t.stats = fs.statSync(tPath)
+  return t
+}
