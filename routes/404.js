1d9b9e0773369f6b34d5c100f38ff299013ac923
Gabriel Farrell <g@grrawr.com>
2014-03-29 11:10:03 -0400

    Content-type for error pages

--- routes/404.js
+++ routes/404.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var hs = require('hyperstream')
 
 module.exports = function (req, res) {
-  res.statusCode = 404;
+  res.writeHead(404, {'Content-Type': 'text/html'})
   fs.createReadStream(__dirname + '/../static/index.html').pipe(hs({
     '.main': 'Not Found'
   })).pipe(res)
--- routes/500.js
+++ routes/500.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var hs = require('hyperstream')
 
 module.exports = function (req, res) {
-  res.statusCode = 500
+  res.writeHead(500, {'Content-Type': 'text/html'})
   fs.createReadStream(__dirname + '/../static/index.html').pipe(hs({
     '.main': 'Server Error'
   })).pipe(res)
