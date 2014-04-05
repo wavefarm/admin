a46d41e7218b861b4fbc8ff0a6f9d4dc8588b647
Gabriel Farrell <g@grrawr.com>
2014-04-05 00:54:37 -0400

    Results for other queries and item display. Refs #26

--- browser/main.js
+++ browser/main.js
@@ -1,44 +1,65 @@
 var api = require('../api')
 var h = require('hyperscript')
 var page = require('page')
+var qs = require('querystring')
 var rels = $('.rel');
 
 // SSE reload
 require('deva');
 
-var pageTitle = $('title')
-var main = $('.main')
+var $pageTitle = $('title')
+var $q = $('#q')
+var $main = $('.main')
 
 page('/', function (ctx) {
   console.log(ctx)
-  api.search(null, function (err, results) {
-    console.log(results)
+  if (ctx.querystring == $main.data('querystring')) {
+    return $('.result').show('slow')
+  }
+  $main.data('querystring', ctx.querystring)
+  var q = qs.parse(ctx.querystring).q
+  $q.val(q)
+  api.search(q, function (err, results) {
+    //console.log(results)
     var count = $('.count')
     if (err) return console.error(err)
-    // If count already exists just update total
-    if (count.length) {
-      count.find('.total').html(results.total)
-    } else {
-      main.html(h('.count', 
-        h('span.total', results.total),
-        h('span', ' results')
-      ))
-    }
+    $main.html(h('.count',
+      h('span.total', results.total),
+      h('span', ' results')
+    ))
     var result
     for (var i = 0; i < results.hits.length; i++) {
       result = results.hits[i]
-      main.append(require('../render/result')(result))
+      $main.append(require('../render/result')(result))
     }
   })
 })
 
 page('/:id', function (ctx) {
   console.log(ctx)
+  if (ctx.state.querystring) {
+    // TODO Load results but keep them hidden
+  } else {
+    // Results already loaded
+    ctx.state.querystring = $main.data('querystring')
+
+    // Hide other results
+    $('.result').not('#' + id).hide('slow')
+  }
+  var id = ctx.params.id
+  var item = $('#' + id)
 })
 
 // Register popstate and click bindings
 page()
 
+// Push state on search form submit
+$('.search').on('submit', function (e) {
+  page('/?q=' + encodeURIComponent($q.val()))
+  e.preventDefault()
+})
+
+// XXX Idea: Modify limit param in URL as infinite scroll grows
 
 rels.each(function (i, rel) {
   rel = $(rel);
--- render/result.js
+++ render/result.js
@@ -1,10 +1,11 @@
 var h = require('hyperscript')
 
 module.exports = function (item) {
-  return h('.result', {'data-item': item},
+  return h('.result', {'id': item.id, 'data-item': JSON.stringify(item)},
     h('h2',
-      h('a', {'href': '/' + item.id}, item.main)
-    ),
-    h('.type', item.type)
+      h('a', {'href': '/' + item.id}, item.main),
+      ' ',
+      h('span.type', '(' + item.type + ')')
+    )
   )
 }
