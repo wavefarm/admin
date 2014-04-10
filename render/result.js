314f18cdac92b7c216d66b24dd2cbd9f7a1c02c4
Gabriel Farrell <g@grrawr.com>
2014-04-10 12:48:15 -0400

    Style changes #26

--- render/result.js
+++ render/result.js
@@ -2,8 +2,8 @@ var h = require('hyperscript')
 
 module.exports = function (item) {
   return h('.result', {'id': item.id, 'data-item': JSON.stringify(item)},
-    h('h2',
-      h('a', {'href': '/' + item.id}, item.main),
+    h('a', {'href': '/' + item.id}, 
+      h('span.item-main', item.main),
       ' ',
       h('span.type', '(' + item.type + ')')
     )
