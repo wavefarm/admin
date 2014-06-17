bbfb77a3f13071442976311c59a9a936e61175c1
Gabriel Farrell <g@grrawr.com>
2014-06-16 21:01:58 -0400

    Truncate related

--- render/widget/rels.js
+++ render/widget/rels.js
@@ -1,13 +1,22 @@
 // Widget for linking to and managing relative items
 var h = require('hyperscript')
 
+
+function trunc (str) {
+  str = str || ''
+  if (str.length > 60) {
+    str = str.substr(0, 60) + '...'
+  }
+  return str
+}
+
 module.exports = function (rels) {
   rels = rels || []
   return h('.rels',
     h('ul',
       rels.map(function (rel) {
         return h('li',
-          h('a', {href: rel.id, target: '_blank'}, rel.main),
+          h('a', {href: rel.id, target: '_blank'}, trunc(rel.main)),
           h('button.fa.fa-unlink', {title: 'unlink'})
         )
       })
