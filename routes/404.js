111876de84d9d341306e4de8fc8b2a0e8674d810
Gabriel Farrell <g@gsf.io>
2014-12-15 03:17:29 -0500

    Drop server and routes. #38

--- routes/404.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-var hs = require('hyperstream')
-
-module.exports = function (req, res) {
-  res.writeHead(404, {'Content-Type': 'text/html'})
-  fs.createReadStream(__dirname + '/../static/index.html').pipe(hs({
-    '.main': 'Not Found'
-  })).pipe(res)
-}
