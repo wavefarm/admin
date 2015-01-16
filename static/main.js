8391617f2cf1b64676dec2578216b8c17da48d38
Gabriel Farrell <g@gsf.io>
2015-01-16 00:48:32 -0500

    Drop jQuery

--- static/main.js
+++ static/main.js
@@ -10,52 +10,62 @@ function search (params, cb) {
     cb = params
     params = null
   }
-  var settings = {
-    url: 'http://api.wavefarm.org/search',
-    success: cb
+
+  var url = 'http://api.wavefarm.org/search'
+  if (params) url += '?' + params
+
+  var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState != 4 || xhr.status != 200) return
+    cb(JSON.parse(xhr.responseText))
   }
-  if (params) settings.data = params
-  $.ajax(settings)
+  xhr.open('GET', url)
+  xhr.send()
 }
 
-var q = $('#q')
-var main = $('#main')
-var total = $('#total')
-var hits = $('#hits')
-var item = $('#item')
+var q = document.getElementById('q')
+var main = document.getElementById('main')
+var total = document.getElementById('total')
+var hits = document.getElementById('hits')
+var item = document.getElementById('item')
 
-// Run search on initial page load
-q.val(initialParamsParsed.q)
-search(initialParams, function (data) {
-  console.log(data)
+function initialize () {
+  q.value = initialParamsParsed.q || ''
 
-  total.text(data.total)
+  // Run search on initial page load
+  search(initialParams, function (data) {
+    console.log(data)
 
-  var i
-  var hit
-  var hitLen = data.hits.length
-  var hitsInner = ''
-  var desc
+    total.innerHTML = data.total
 
-  for (i=0; i < hitLen; i++) {
-    hit = data.hits[i]
-    desc = hit.description || hit.briefDescription || hit.longDescription || ''
+    var i
+    var hit
+    var hitLen = data.hits.length
+    var hitsInner = ''
+    var desc
 
-    // Strip HTML tags from description for excerpt display
-    desc = desc.replace(/<[^>]*>/g, '')
+    for (i=0; i < hitLen; i++) {
+      hit = data.hits[i]
+      desc = hit.description || hit.briefDescription || hit.longDescription || ''
 
-    hitsInner = hitsInner
-      + '<div class="hit" id="' + hit.id + '">'
-      +   '<a href="/' + hit.id + '">'
-      +     '<h3><span class="hit-main">' + hit.main + '</span> '
-      +       '<span class="hit-type">' + hit.type + '</span>'
-      +     '</h3>'
-      +     '<div class="credit">' + hit.credit + '</div>'
-      +     '<div class="description">'
-      +       (desc.length > 60 ? desc.substr(0, 60) + '...' : desc)
-      +     '</div>'
-      +   '</a>'
-      + '</div>'
-  }
-  hits.html(hitsInner)
-})
+      // Strip HTML tags from description for excerpt display
+      desc = desc.replace(/<[^>]*>/g, '')
+
+      hitsInner = hitsInner
+        + '<div class="hit" id="' + hit.id + '">'
+        +   '<a href="/' + hit.id + '">'
+        +     '<h3><span class="hit-main">' + hit.main + '</span> '
+        +       '<span class="hit-type">' + hit.type + '</span>'
+        +     '</h3>'
+        +     '<div class="credit">' + hit.credit + '</div>'
+        +     '<div class="description">'
+        +       (desc.length > 60 ? desc.substr(0, 60) + '...' : desc)
+        +     '</div>'
+        +   '</a>'
+        + '</div>'
+    }
+    hits.innerHTML = hitsInner
+  })
+}
+
+initialize()
