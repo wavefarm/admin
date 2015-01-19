c266aea69fe4afba1d586aeb224d9d95563fa9c9
Gabriel Farrell <g@gsf.io>
2015-01-19 16:11:31 -0500

    Item rendering. #41

--- static/main.js
+++ static/main.js
@@ -1,11 +1,21 @@
 'use strict'
 
-function getParams () {
-  return window.location.search.substr(1)
-}
+var qEl = document.getElementById('q')
+var mainEl = document.getElementById('main')
+var totalEl = document.getElementById('total')
+var itemEl = document.getElementById('item')
 
-var initialParams = getParams()
-var initialParamsParsed = queryString.parse(initialParams)
+function getItem (itemId, cb) {
+  var url = 'http://api.wavefarm.org/' + itemId
+
+  var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState != 4 || xhr.status != 200) return
+    cb(JSON.parse(xhr.responseText))
+  }
+  xhr.open('GET', url)
+  xhr.send()
+}
 
 function search (params, cb) {
   if (!cb) {
@@ -25,80 +35,113 @@ function search (params, cb) {
   xhr.send()
 }
 
-var q = document.getElementById('q')
-var main = document.getElementById('main')
-var total = document.getElementById('total')
-var hits = document.getElementById('hits')
-var item = document.getElementById('item')
+function renderFullItem (item) {
+  var itemEl
+  var itemHeader
+  var itemMain
+  var itemType
+  var itemForm
+  var publicLink
+  var publicUrl
 
-function populateHits () {
+  itemEl = document.createElement('a')
+  mainEl.appendChild(itemEl)
+  itemEl.className = 'item'
+
+  publicUrl = 'wavefarm.org/archive/' + item.id
+  publicLink = document.createElement('a')
+  itemEl.appendChild(publicLink)
+  publicLink.className = 'action public'
+  publicLink.href = '//' + publicUrl
+  publicLink.target = '_blank'
+  publicLink.title = 'public location'
+  publicLink.appendChild(document.createTextNode(publicUrl))
+
+  itemHeader = document.createElement('h3')
+  itemEl.appendChild(itemHeader)
+  itemMain = document.createElement('span')
+  itemHeader.appendChild(itemMain)
+  itemMain.className = 'item-main'
+  itemMain.appendChild(document.createTextNode(item.main))
+  itemHeader.appendChild(document.createTextNode(' '))
+  itemType = document.createElement('span')
+  itemHeader.appendChild(itemType)
+  itemType.className = 'item-type'
+  itemType.appendChild(document.createTextNode(item.type))
+}
+
+function renderItems (items) {
+  var desc
+  var item
+  var itemCredit
+  var itemDesc
+  var itemEl
+  var itemHeader
+  var itemMain
+  var itemType
+  var itemLen
+  var i
+
+
+  itemLen = items.length
+  for (i=0; i < itemLen; i++) {
+    item = items[i]
+    desc = item.description || item.briefDescription || item.longDescription || ''
+
+    // Strip HTML tags from description and truncate for excerpt
+    desc = desc.replace(/<[^>]*>/g, '')
+    desc = desc.length > 60 ? desc.substr(0, 60) + '...' : desc
+
+    itemEl = document.createElement('a')
+    itemEl.className = 'item'
+    itemEl.id = item.id
+    itemEl.href = '/' + item.id
+
+
+    itemHeader = document.createElement('h3')
+    itemMain = document.createElement('span')
+    itemMain.className = 'item-main'
+    itemMain.appendChild(document.createTextNode(item.main))
+    itemHeader.appendChild(itemMain)
+    itemHeader.appendChild(document.createTextNode(' '))
+    itemType = document.createElement('span')
+    itemType.className = 'item-type'
+    itemType.appendChild(document.createTextNode(item.type))
+    itemHeader.appendChild(itemType)
+    itemEl.appendChild(itemHeader)
+
+    itemCredit = document.createElement('div')
+    itemCredit.className = 'credit'
+    itemCredit.appendChild(document.createTextNode(item.credit || ''))
+    itemEl.appendChild(itemCredit)
+
+    itemDesc = document.createElement('div')
+    itemDesc.className = 'description'
+    itemDesc.appendChild(document.createTextNode(desc))
+    itemEl.appendChild(itemDesc)
+
+    mainEl.appendChild(itemEl)
+  }
 }
 
 function initialize () {
-  q.value = initialParamsParsed.q || ''
+  var initialParams
+  var initialParamsParsed
+  var itemId = /\w{6}/.exec(window.location.pathname)
+  if (itemId) {
+    getItem(itemId, function (item) {
+      renderFullItem(item)
+    })
+  } else {
+    initialParams = window.location.search.substr(1)
+    initialParamsParsed = queryString.parse(initialParams)
 
-  // Run search on initial page load
-  search(initialParams, function (data) {
-    console.log(data)
-
-    total.textContent = total.innerText = data.total
-
-    main.removeChild(hits)
-
-    var desc
-    var hit
-    var hitCredit
-    var hitDesc
-    var hitEl
-    var hitHeader
-    var hitMain
-    var hitType
-    var hitLen = data.hits.length
-    var i
-    var wrapperLink
-
-    for (i=0; i < hitLen; i++) {
-      hit = data.hits[i]
-      desc = hit.description || hit.briefDescription || hit.longDescription || ''
-
-      // Strip HTML tags from description and truncate for excerpt
-      desc = desc.replace(/<[^>]*>/g, '')
-      desc = desc.length > 60 ? desc.substr(0, 60) + '...' : desc
-
-      hitEl = document.createElement('div')
-      hitEl.className = 'hit'
-      hitEl.id = hit.id
-
-      wrapperLink = document.createElement('a')
-      wrapperLink.href = '/' + hit.id
-
-      hitHeader = document.createElement('h3')
-      hitMain = document.createElement('span')
-      hitMain.className = 'hit-main'
-      hitMain.appendChild(document.createTextNode(hit.main))
-      hitHeader.appendChild(hitMain)
-      hitHeader.appendChild(document.createTextNode(' '))
-      hitType = document.createElement('span')
-      hitType.className = 'hit-type'
-      hitType.appendChild(document.createTextNode(hit.type))
-      hitHeader.appendChild(hitType)
-      wrapperLink.appendChild(hitHeader)
-
-      hitCredit = document.createElement('div')
-      hitCredit.className = 'credit'
-      hitCredit.appendChild(document.createTextNode(hit.credit || ''))
-      wrapperLink.appendChild(hitCredit)
-
-      hitDesc = document.createElement('div')
-      hitDesc.className = 'description'
-      hitDesc.appendChild(document.createTextNode(desc))
-      wrapperLink.appendChild(hitDesc)
-
-      hitEl.appendChild(wrapperLink)
-      hits.appendChild(hitEl)
-    }
-    main.appendChild(hits)
-  })
+    qEl.value = initialParamsParsed.q || ''
+    search(initialParams, function (data) {
+      totalEl.textContent = totalEl.innerText = data.total
+      renderItems(data.hits)
+    })
+  }
 }
 
 initialize()
